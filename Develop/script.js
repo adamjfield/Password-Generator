20823989f95248341303494ecf1f6a9fdf59ebaf
Adam Field <adamjfield@gmail.com>
2021-10-01 15:25:06 -0500

    added pseidocode

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,24 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+// when the user selects generate password button a alert window appears to ask a series of password criteria
+
+// user type length they wish password to be between 8 & 128 characters
+
+// user then prompted with choice of upper case letters, lower case letters, and special characters
+// which they can choose to include or not include
+
+// at least one character type must be selected
+// if one is not they show alert informing user one must be selected
+
+// once user has selected their preferred password criteria a alert window will confirm their selections
+
+// once user has confirmed their password criteria the function will generate and randomize the password
+
+// the password is then returned to writePassword function to be displayed
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
