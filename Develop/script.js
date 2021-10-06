dceb0a8c728337feb61bd8e00ba7534bfe96a893
Adam Field <adamjfield@gmail.com>
2021-10-05 23:06:16 -0500

    added confirm boxes for characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,29 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var passwordLength;
+var characterLower = randomLower();
+var characterUpper = randomUpper();
+var characterNumber = randomNumber();
+var characterSpecial = randomSpecial();
 
-function characterLower() {
+function randomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
+}
 
-function characterUpper() {
+function randomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-};
+}
 
-function characterNumber() {
+function randomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 9) + 49);
-};
+}
 
-function characterSpecial() {
-  const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+function randomSpecial() {
+  const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
   return symbols[Math.floor(Math.random() * symbols.length)];
-};
+}
 
-console.log(characterLower(), characterUpper(), characterNumber(), characterSpecial())
+console.log(characterLower, characterUpper, characterNumber, characterSpecial);
 
 // Write password to the #password input
 function writePassword() {
@@ -33,6 +37,7 @@ function writePassword() {
 
 function generatePassword() {
   // when the user selects generate password button a
+  // user types length they wish password to be between 8 & 128 characters
   passwordLength = prompt(
     "How many characters would you like your password to contain? Must be between 8-128 characters."
   );
@@ -42,16 +47,27 @@ function generatePassword() {
     alert("Must enter a value between 8 - 128!");
   }
   // alert window appears to ask a series of password criteria
-
-  // user types length they wish password to be between 8 & 128 characters
-
   // user then prompted with choice of upper case letters,
-  // lower case letters, and special characters
-
+  // lower case letters, numbers, and special characters
   // which they can choose to include or not include
-
+  else {
+    confirmSpecial = confirm(
+      "Click OK to confirm including special characters."
+    );
+    confirmNumber = confirm(
+      "Click OK to confirm including numeric characters."
+    );
+    confirmLower = confirm(
+      "Click OK to confirm including lowercase characters."
+    );
+    confirmUpper = confirm(
+      "Click OK to confirm including uppercase characters."
+    );
+  }
   // at least one character type must be selected
-
+  if (!confirmSpecial && !confirmNumber && !confirmLower && !confirmUpper) {
+    alert("Must select at least one character type!");
+  }
   // if one is not they show alert informing user one must be selected
 
   // once user has selected their preferred password criteria a
